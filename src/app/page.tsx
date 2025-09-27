c35603d3ccd32eb448e5d1593af922ce655ec521
Fábio Milagres <fabioemanuellmilagres@gmail.com>
2025-09-27 15:21:53 -0300

    Add GallerySection component with image carousel and modal functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import AboutSection from "@/components/about-section";
 import BannerSection from "@/components/banner-section";
 import FeaturesSection from "@/components/features-section";
+import GallerySection from "@/components/gallery-section";
 import Header from "@/components/header/header";
 import PlansSection from "@/components/plans-section";
 
@@ -12,6 +13,7 @@ export default function Home() {
       <AboutSection />
       <PlansSection />
       <FeaturesSection />
+      <GallerySection />
     </div>
   );
 }
