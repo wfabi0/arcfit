bf65a1e1b72763108abe06c852157f40e103c89c
Fábio Milagres <fabioemanuellmilagres@gmail.com>
2025-09-29 02:40:11 -0300

    chore: update drizzle configuration and dependencies

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -4,11 +4,12 @@ import { defineConfig } from "drizzle-kit";
 const DATABASE_URL = process.env.DATABASE_URL as string;
 
 export default defineConfig({
+  dialect: "postgresql",
   schema: "./src/db/schema/index.ts",
   out: "./src/db/migrations",
-  driver: "pg",
+  driver: "pglite",
   dbCredentials: {
-    connectionString: DATABASE_URL,
+    url: DATABASE_URL,
   },
   verbose: true,
   strict: true,
